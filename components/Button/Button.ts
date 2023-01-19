9eaf692b3dc19dd5f16fc7844d75e11fd1885649
alex5667 <alex56671111@gmail.com>
2023-01-19 22:53:20 +0200

    feat: add item, car

---
+++ components/Button/Button.ts
@@ -0,0 +1,31 @@
+import render from '../../utils/render';
+import './Button.scss';
+
+export class Button {
+  onClick: () => void = () => {};
+  button: HTMLButtonElement;
+
+  constructor(
+    parent: HTMLElement,
+    classNames: string[] = [],
+    label: string,
+    disabled = false,
+  ) {
+    this.button = render<HTMLButtonElement>(parent, 'button', ['btn'], label);
+
+    this.button.classList.add(...classNames);
+    this.button.addEventListener('click', () => this.onClick());
+
+    if (disabled) {
+      this.setDisabled(true);
+    }
+  }
+
+  setDisabled(type = false): void {
+    this.button.toggleAttribute('disabled', type);
+  }
+
+  removeDisabled(): void {
+    this.button.removeAttribute('disabled');
+  }
+}
--- components/Garage/GarageItem/GarageItem.ts
+++ components/Garage/GarageItem/GarageItem.ts
@@ -1,15 +1,64 @@
 import render from '../../../utils/render';
 import { ICar } from '../../../interfaces';
+import { Button } from '../../Button/Button';
+import { CarImage } from '../../CarImage/CarImage';
 
 import './GarageItem.scss';
 
 export class GarageItem {
   public car: ICar;
+  private itemContainer: HTMLDivElement;
+  private startEngineCar: Button;
+  private stopEngineCar: Button;
+  private imageCar: HTMLDivElement;
+  removeCar: (carId: number) => void = () => {};
+  updateCar: (carId: number) => void = () => {};
+  startEngine: (carId: number) => void = () => {};
+  stopEngine: () => void = () => {};
 
   constructor(parent: HTMLElement, car: ICar) {
     this.car = car;
-    const itemContainer = render<HTMLDivElement>(parent, 'div', ['garage__item']);
-    const carTop = render<HTMLDivElement>(itemContainer, 'div', ['item__top']);
+    this.itemContainer = render<HTMLDivElement>(parent, 'div', [
+      'garage__item',
+    ]);
+    const containerTop = render<HTMLDivElement>(this.itemContainer, 'div', [
+      'item__top',
+    ]);
 
+    const selectBtn = new Button(containerTop, ['btn-top'], 'Select');
+    selectBtn.onClick = () => {
+      if (car.id) this.updateCar(car.id);
+    };
+
+    const removeBtn = new Button(containerTop, ['btn-top'], 'Remove');
+    removeBtn.onClick = () => {
+      if (car.id) this.removeCar(car.id);
+      this.itemContainer.remove();
+    };
+
+    render<HTMLDivElement>(containerTop, 'span', ['span__top'], car.name);
+
+    const containerBottom = render<HTMLDivElement>(this.itemContainer, 'div', [
+      'item__bottom',
+    ]);
+
+    const bottomButtons = render<HTMLDivElement>(containerBottom, 'div', [
+      'bottom__buttons',
+    ]);
+
+    this.startEngineCar = new Button(bottomButtons, ['btn-bottom'], 'A');
+    this.startEngineCar.onClick = () => {};
+    this.stopEngineCar = new Button(bottomButtons, ['btn-bottom'], 'B', true);
+    this.stopEngineCar.onClick = () => {};
+
+    this.imageCar = render<HTMLDivElement>(containerBottom, 'div', [
+      'image-car',
+    ]);
+    this.imageCar.innerHTML = CarImage(car.color);
+
+    const finishFlag = render<HTMLImageElement>(containerBottom, 'img', [
+      'finish-flag',
+    ]);
+    finishFlag.setAttribute('src', '../../../assets/flag-svgrepo-com.svg');
   }
 }
